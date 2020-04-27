3dc72659e8ad9424b1ac0fa29765058a6ea7c280
vijay58 <pvk5819@gmail.com>
2020-04-27 17:20:04 +0000

    add src code

---
+++ src/main/java/com/aws/springboot/awsSpringBootdemo/AwsSpringBootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.springboot.awsSpringBootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringBootdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/springboot/awsSpringBootdemo/AwsSpringBootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.springboot.awsSpringBootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringBootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
