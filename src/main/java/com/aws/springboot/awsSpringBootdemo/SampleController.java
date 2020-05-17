b4b56c6ff24df2200225c1d6615832c25b67ea72
vkumar <vkumar@1worldsync.com>
2020-05-17 10:45:30 +0530

    added SampleController.class

---
+++ src/main/java/com/aws/springboot/awsSpringBootdemo/SampleController.java
@@ -0,0 +1,15 @@
+package com.aws.springboot.awsSpringBootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class SampleController {
+
+    @RequestMapping("/info")
+    public String getSampleOutput() {
+        return "sample spring boot Application";
+    }
+
+}
